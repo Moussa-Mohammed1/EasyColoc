345e354ccad1b510bda138fb2eeb251f0ec6274a
Moussa-Mohammed1 <moussamohammed2005@gmail.com>
2026-02-26 09:39:12 +0000

    feat(migrations) : add the initial project migrations including colocations, invitations, payments, memberships, expenses and categories

---
+++ database/migrations/2026_02_25_141840_create_payments_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('amount');
+            $table->timestamp('paid_at')->nullable();
+            $table->foreignId('payer_id')->constrained('users');
+            $table->foreignId('expense_id')->constrained('expenses');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
