0dff4a6766864e71f3acebdaf58a80836cc485c7
Moussa-Mohammed1 <moussamohammed2005@gmail.com>
2026-02-27 15:19:27 +0000

    feat(model): create Membership model with relationships to User and Colocation

---
+++ app/Models/Membership.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Membership extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'colocation_id',
+        'role',
+        'left_at',
+    ];
+
+    protected $casts = [
+        'left_at' => 'datetime',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function colocation()
+    {
+        return $this->belongsTo(Colocation::class);
+    }
+}
