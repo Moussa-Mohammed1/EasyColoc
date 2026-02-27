10e7f648293faa5136db1138e461d444b7227643
Moussa-Mohammed1 <moussamohammed2005@gmail.com>
2026-02-27 15:19:09 +0000

    feat(model): create Expense model with relationships to User, Category, and Payment

---
+++ app/Models/Expense.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Expense extends Model
+{
+    protected $fillable = [
+        'title',
+        'amount',
+        'user_id',
+        'category_id'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'category_id');
+    }
+
+    public function payments()
+    {
+        return $this->hasMany(Payment::class);
+    }
+}
