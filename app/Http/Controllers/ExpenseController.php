64e78c6470e200188906c4bc3afddad6dd12ddb9
Moussa-Mohammed1 <moussamohammed2005@gmail.com>
2026-02-27 15:10:48 +0000

    feat(controller): add ExpenseController with CRUD methods

---
+++ app/Http/Controllers/ExpenseController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreExpenseRequest;
+use App\Models\Expense;
+use Illuminate\Http\Request;
+
+class ExpenseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreExpenseRequest $request)
+    {
+        Expense::create($request->validated());
+        return redirect()->back();
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Expense $expense)
+    {
+        $expense->delete();
+        return redirect()->back();
+    }
+}
