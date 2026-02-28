0269af937040128b45712e05007ede40f2056dcf
Moussa-Mohammed1 <moussamohammed2005@gmail.com>
2026-02-28 16:25:02 +0000

    feat(routes): update dashboard route to use controller and add resource routes for colocations, expenses, and categories

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,13 @@
 <?php
 
+use App\Http\Controllers\ColocationController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ExpenseController;
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
 });
 
 Route::middleware([
@@ -11,7 +15,8 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+    Route::get('/dashboard',  [DashboardController::class, 'index'])->name('dashboard.index');
+    Route::resource('colocations', ColocationController::class);
+    Route::resource('expenses', ExpenseController::class);
+    Route::resource('categories', CategoryController::class);
 });
