35b7a006a9cf8196972bf7ddf32b7cd5346acd63
Moussa-Mohammed1 <moussamohammed2005@gmail.com>
2026-03-01 16:08:54 +0000

    feat(routes): update routes to include invitation handling and payment resource

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,16 @@ use App\Http\Controllers\ColocationController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ExpenseController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\InvitationController;
+use App\Http\Controllers\PaymentController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('dashboard');
+    return view(view: 'welcome');
 });
+
+Route::get('/invitations/{token}', [InvitationController::class, 'accept'])->name('invitations.accept');
 
 Route::middleware([
     'auth:sanctum',
@@ -19,4 +24,9 @@ Route::middleware([
     Route::resource('colocations', ColocationController::class);
     Route::resource('expenses', ExpenseController::class);
     Route::resource('categories', CategoryController::class);
+    Route::resource('payments', PaymentController::class);
+    
+    Route::post('/invitations', [InvitationController::class, 'store'])->name('invitations.store');
+    Route::post('/invitations/{token}', [InvitationController::class, 'process'])->name('invitations.process');
+    Route::post('/member/remove', [UserController::class, 'remove'])->name('user.remove');
 });
